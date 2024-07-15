5552ca218170c8c508d224d3819023f4356d3977
Amira Ashraf <amiraashraf0210@gmail.com>
2024-07-15 11:24:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace first_project
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter an array (comma-separated values):");
+            string input = Console.ReadLine();
+            var number = input.Split(new[] { ", " }, StringSplitOptions.None);
+            int[] arr = new int[number.Length];
+            for (int i = 0; i < number.Length; i++)
+            {
+                int result = 1;
+                for (int j = 0; j < number.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        result = result * int.Parse(number[j]);
+                    }
+                }
+                arr[i] = result;
+            }
+            for (int a = 0; a < number.Length; a++)
+            {
+                Console.Write(arr[a] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
